1cf02996a55f22c4e8ff9680642fe0775c8d7f64
shanajasila <shanajasilam@gmail.com>
2022-12-06 17:05:41 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-course/view-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-course',
+  templateUrl: './view-course.component.html',
+  styleUrls: ['./view-course.component.css']
+})
+export class ViewCourseComponent {
+
+  
+}
